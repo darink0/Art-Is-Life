6ca068a9c01de28c6420e8fec84078f6c1190e87
darink0 <136697263+darink0@users.noreply.github.com>
2023-06-24 19:16:06 +0700

    Resolve compatibility issues

---
+++ valley_zebra_nature.rb
@@ -0,0 +1,185 @@
+#!/usr/bin/ruby
+
+# Commenting out code to make it more readable
+
+# This code will output the phrase 'Art Is Life'
+
+# Prints a message to the console
+puts "Art Is Life"
+
+# Prints the phrase to string
+str = "Art Is Life"
+puts str
+
+# Prints the phrase at the end of the program
+END {
+  puts "Art Is Life"
+}
+
+# Creates a hash with the phrase as the value
+my_hash = { phrase: "Art Is Life" }
+puts my_hash[:phrase]
+
+# Creates an array with the phrase as an element
+array = ["Art Is Life"]
+puts array[0]
+
+# Creates a loop that will print the phrase
+n = 10
+n.times do 
+  puts "Art Is Life"
+end
+
+# Creates a method that prints the phrase
+def print_phrase 
+  puts "Art Is Life"
+end
+
+# Calls the method to print the phrase
+print_phrase
+
+# Creates a class with a method to print the phrase
+class Phrase 
+  def say_phrase 
+    puts "Art Is Life"
+  end
+end
+
+# Creates an object from the class and calls the method to print the phrase
+my_phrase = Phrase.new
+my_phrase.say_phrase
+
+# Creates an if statement which will print the phrase
+if 1 == 1
+  puts "Art Is Life"
+end
+
+# Creates a case statement which will print the phrase
+case
+when 1 == 1
+  puts "Art Is Life"
+else
+  puts "Nothing to see here"
+end
+
+# Creates a loop that prints the phrase multiple times
+i = 0
+while i < 5
+  puts "Art Is Life"
+  i += 1
+end
+
+# Creates a loop that prints the phrase 10 times
+10.times do
+  puts "Art Is Life"
+end
+
+# Creates a method which prints the phrase
+def printhorse
+  puts "Art Is Life"
+end
+
+# Calls the method to print the phrase
+printhorse
+
+# Creates a class with a method to print the phrase
+class Print_Phrase
+  def printme
+    puts "Art Is Life"
+  end
+end
+
+# Creates an object of the class and calls the method to print the phrase
+message = Print_Phrase.new
+message.printme
+
+# Creates a loop that prints the phrase 5 times
+5.times do 
+  puts "Art Is Life"
+end
+
+# Creates a loop that prints the phrase 10 times
+10.times do
+  puts "Art Is Life"
+end
+
+# Creates a method to print the phrase
+def phrase_printer
+  puts "Art Is Life"
+end
+
+# Calls the method to output the phrase
+phrase_printer
+
+# Creates a hash with the phrase as the value
+my_hash = { phrase: "Art Is Life" }
+puts my_hash[:phrase]
+
+# Creates an array with the phrase as an element
+array = ["Art Is Life"]
+puts array[0]
+
+# Creates a method to print the phrase
+def phrase_print
+  puts "Art Is Life"
+end
+
+# Calls the method to print the phrase
+phrase_print
+
+# Creates a loop that prints the phrase 5 times
+5.times do
+  puts "Art Is Life"
+end
+
+# Creates a loop that prints the phrase 10 times
+10.times do
+  puts "Art Is Life"
+end
+
+# Creates a method that will print the phrase
+def printphrase
+  puts "Art Is Life"
+end
+
+# Calls the method to output the phrase
+printphrase
+
+# Creates a case statement that prints the phrase
+case
+when 1 == 1
+  puts "Art Is Life"
+else
+  puts "Nothing to see here"
+end
+
+# Creates an if statement which prints the phrase
+if 1 == 1
+  puts "Art Is Life"
+end
+
+# Creates a class with a method to print the phrase
+class Phrase
+  def input_phrase
+    puts "Art Is Life"
+  end
+end
+
+# Creates an object from the class and calls the method to print the phrase
+my_phrase = Phrase.new
+my_phrase.input_phrase
+
+# Prints the phrase at the end of the program
+END {
+  puts "Art Is Life"
+}
+
+# Creates a loop that prints the phrase
+n = 10
+n.times do 
+  puts "Art Is Life"
+end
+
+# Prints the phrase to a string
+str = "Art Is Life"
+puts str
